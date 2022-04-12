b9dd54bf9b88307831abc863ff07a9458465885a
aurel <appiusa@student.ethz.ch>
2022-04-12 12:38:03 +0200

    initial commit

---
+++ app/publisher.cpp
@@ -0,0 +1,6 @@
+#include <iostream>
+int main(int argc, char *argv[])
+{
+    std::cout << "publisher" << std::endl;
+    return 0;
+}
---
+++ app/subscriber.cpp
@@ -0,0 +1,6 @@
+#include <iostream>
+int main(int argc, char *argv[])
+{
+    std::cout << "subscriber" << std::endl;
+    return 0;
+}
